adea72c3c17c0c344e7e1b5b88536bc11cb3fe04
hisamatsu yoshiyuki <hisasann25@gmail.com>
2021-09-14 22:18:13 +0900

    chore(blog.config.js): change blog name to my name

--- blog.config.js
+++ blog.config.js
@@ -1,10 +1,10 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  title: 'dialy.hisasann.dev',
+  author: 'Yoshiyuki Hisamatsu',
+  email: 'hisasann25@gmail.com',
+  link: 'https://dialy.hisasann.dev/',
+  description: 'Yoshiyuki Hisamatsu',
+  lang: 'ja-JP', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
@@ -16,8 +16,8 @@ const BLOG = {
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
-  ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/craigaryhart',
+  ogImageGenerateURL: 'https://hisasann.dev/assets/images/hisasann_400x400.jpg', // The link to generate OG image, don't end with a slash
+  socialLink: 'https://twitter.com/hisasann',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
