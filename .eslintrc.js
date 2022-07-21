60dedfe165932f8806987035552b36694d84e0ec
hisamatsu yoshiyuki <hisasann25@gmail.com>
2022-07-21 20:01:59 +0900

    chore(next): update next.js to 12.2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,6 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'plugin:@next/next/recommended',
-    'standard'
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 12,
-    sourceType: 'module'
-  },
-  plugins: ['react'],
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
+  extends: ['next/core-web-vitals'],
   rules: {
-    'react/prop-types': 'off'
+    'react/display-name': 'off',
   },
-  globals: {
-    React: true
-  }
 }
