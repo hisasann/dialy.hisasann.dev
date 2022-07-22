8f45fb88de873f078c8aca86efef45676f41a65e
hisamatsu yoshiyuki <hisasann25@gmail.com>
2022-07-22 11:43:33 +0900

    feat(blog): change postsPerPage from 100 to 30

--- blog.config.js
+++ blog.config.js
@@ -11,7 +11,7 @@ const BLOG = {
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // If leave this empty, current year will be used.
-  postsPerPage: 100,
+  postsPerPage: 30,
   sortByDate: false,
   showAbout: true,
   showArchive: true,
